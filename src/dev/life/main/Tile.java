66dbc14e24018f77e73749bf4d7dde4bef23dad9
jrgivre <jrgivre@gmail.com>
2020-07-22 20:02:27 -0400

    Finished updateGrid

--- src/dev/life/main/Tile.java
+++ src/dev/life/main/Tile.java
@@ -1,5 +1,21 @@
 package dev.life.main;
 
-public class Tile {
-
+public class Tile 
+{
+	private boolean bAlive;
+	
+	public Tile()
+	{
+		bAlive = false;
+	}
+	
+	public boolean getAlive()
+	{
+		return this.bAlive;
+	}
+	
+	public void setAlive(boolean bAlive)
+	{
+		this.bAlive = bAlive;
+	}
 }
